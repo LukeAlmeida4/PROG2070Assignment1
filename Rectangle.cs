84ec8039051020b181441b7ba322d964bd67238c
Luke Almeida <Luke-Almeida@hotmail.com>
2018-02-03 11:53:09 -0500

    Initial Commit
    Initial commit for SQA NUNIT assignment 1

---
+++ Rectangle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG2070Assignment1
+{
+    class Rectangle
+    {
+        private int length;
+        private int width;
+
+    }
+}
